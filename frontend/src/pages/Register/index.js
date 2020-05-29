d1d4c5dd448cb7d8d3b4619afbabfa159292c142
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-05-29 18:00:47 -0300

    Conectando frontend com backend (página 'Register')

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 
+import api from '../../services/api'
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 export default function Register() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [whatsapp, setWhatsapp] = useState('')
+    const [city, setCity] = useState('')
+    const [uf, setUf] = useState('')
+
+    const history = useHistory()
+    
+    async function handleRegister(event){
+        event.preventDefault()
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        }
+
+        try{
+            const response = await api.post('ongs', data)
+
+            alert(`Seu ID de acesso: ${response.data.id}`)
+            history.push('/')
+
+        } catch (err) {
+            alert("Erro no cadastro, tente novamente")
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,14 +53,46 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="E-mail"/>
-                    <input placeholder="Whatsapp"/>
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="Nome da ONG"
+                        pattern="[A-Za-z]+\s[A-Za-z]+"
+                        title="Preencha com seu nome e sobrenome, sem acentos"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        required
+                    />  
+                    <input type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                    />
+                    <input placeholder="Whatsapp"
+                        type="number"
+                        title="Preencha com o seu Whatsapp"
+                        value={whatsapp}
+                        onChange={(event) => setWhatsapp(event.target.value)}
+                        required
+                    />
 
                     <div className="input-group">
-                        <input className="input-cidade" placeholder="Cidade"/>
-                        <input className="input-uf" placeholder="UF"/>
+                        <input className="input-cidade"
+                            placeholder="Cidade"
+                            pattern="[A-Za-z\s]+"
+                            title="Preencha com o nome de sua cidade"
+                            value={city}
+                            onChange={(event) => setCity(event.target.value)}
+                            required
+                        />
+                        <input className="input-uf"
+                            placeholder="UF"
+                            pattern="[A-Z]{2}"
+                            title="Preencha com a sigla de seu estado, ex: SP"
+                            value={uf}
+                            onChange={(event) => setUf(event.target.value)}
+                            required
+                        />
                     </div>
 
                     <button className="spec-button" type="submit">Cadastrar</button>
