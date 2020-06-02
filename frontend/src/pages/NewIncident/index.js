f4c71bf03e9358ee0263f273f1264b003524eabc
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-06-02 14:51:14 -0300

    Conectando frontend com backend (página 'NewIncident')

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+
+    const ongId = localStorage.getItem('ongId')
+
+    async function handleNewIncident(event) {
+        event.preventDefault()
+
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        try {
+            api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            })
+
+            alert("Caso cadastrado com sucesso")
+
+        } catch (err) {
+            alert("Erro ao cadastrar novo caso, tente novamente")
+        }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -22,13 +53,27 @@ export default function NewIncident() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título do caso"/>
-                    <textarea placeholder="Descrição"/>
-                    <input placeholder="Valor em reais"/>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Título do caso"
+                        onChange={(event) => setTitle(event.target.value)}
+                        required
+                    />
 
-                    <div className="button-group">
-                        
+                    <textarea
+                        placeholder="Descrição"
+                        onChange={(event) => setDescription(event.target.value)}
+                        required
+                    />
+
+                    <input
+                        placeholder="Valor em reais"
+                        type="number"
+                        onChange={(event) => setValue(event.target.value)}
+                        required
+                    />
+
+                    <div className="button-group">   
                         <button className="spec-button" type="submit">Cadastrar</button>
                     </div>
                 </form>
