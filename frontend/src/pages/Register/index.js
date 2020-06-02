a0777a7d0f50493e6be9e6b0e565421224f09d6f
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-06-01 21:13:30 -0300

    Conectando frontend com backend (página 'Logon')

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -3,6 +3,7 @@ import {Link, useHistory} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 
 import api from '../../services/api'
+
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
