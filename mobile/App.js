55cbe04fcca4d4c2ab0dc77b28dbdf282213b217
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-06-04 14:15:04 -0300

    Configurando rotas para o mobile, instlando pacotes para confirguração dessas rotas

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testando</Text>
-    </View>
+    <Routes />
   )
 }
---
+++ mobile/src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const AppStack = createStackNavigator()
+
+import Incidents from './pages/Incidents/index'
+import Detail from './pages/Detail/index'
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+
+            <AppStack.Navigator screenOptions={{headerShown: false}}>
+
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Detail" component={Detail} />
+                
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    )
+}
