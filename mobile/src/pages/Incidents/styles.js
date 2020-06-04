3455e02d5c8de1bcae2e4d0d0f70d7c19b90e399
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-06-04 11:25:18 -0300

    Subindo estrutura básica do mobile (em reactNative), segunda tentativa

---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,8 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  })  
